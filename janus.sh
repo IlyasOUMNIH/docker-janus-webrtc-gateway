8fd4313202308832e09d6e6165c90dd0538c3323
IlyasOUMNIH <ilyas.oumnih@gmail.com>
2020-03-25 11:29:03 +0100

    janus_gateway_v091

--- janus.sh
+++ janus.sh
@@ -1,6 +1,6 @@
 cd ~
-git clone https://github.com/meetecho/janus-gateway.git
-cd janus-gateway
+git clone https://github.com/IlyasOUMNIH/janus_gateway_v091.git
+cd janus_gateway_v091
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt --enable-docs
 make
