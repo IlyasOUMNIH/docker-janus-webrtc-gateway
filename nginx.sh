b22db9cb61f63d1e41c000e42e405214bbf2b456
IlyasOUMNIH <ilyas.oumnih@gmail.com>
2020-04-20 13:38:36 +0200

    Merging RTMP nginx stream with Janus NGINX

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,15 @@
 apt-get install -y nginx
+apt-get install -y libnginx-mod-rtmp
 cp ./default /etc/nginx/sites-enabled/default
 cp ./janus.js /opt/janus/share/janus/demos/janus.js
+echo $'
+# RTMP Stream
+rtmp {
+    server {
+        listen 1935;
+        chunk_size 4000;
+        application live {
+            live on;
+        }
+    }
+}' >> /etc/nginx/nginx.conf
