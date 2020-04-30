289d9d2c111e8e1ec609f62315b8056aadcf6ce6
IlyasOUMNIH <ilyas.oumnih@gmail.com>
2020-04-30 15:27:14 +0200

    remove git checkout to take the latest version

--- janus.sh
+++ janus.sh
@@ -1,7 +1,6 @@
 cd ~
 git clone https://github.com/IlyasOUMNIH/janus_gateway_v091.git
 cd janus_gateway_v091
-git checkout bbb68f4ae1b8b1a0f613e03f0aa7194bc24a7a2c
 sh autogen.sh
 ./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt
 make
